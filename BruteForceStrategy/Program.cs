01f37645da3491b31788015f07b09433a828b183
eslam hassan <hassaneslam612@gmail.com>
2025-05-16 18:13:58 +0300

    Add project files.

---
+++ BruteForceStrategy/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BruteForceStrategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = [1,2,3,5,6,7];
+            int target = 11;
+            var result = Contains(numbers, target);
+            Console.WriteLine($"'{target} {(result ? "Found":"Not Found")}'");
+        }
+
+        static bool Contains(int[] source , int target)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source[i] == target)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
